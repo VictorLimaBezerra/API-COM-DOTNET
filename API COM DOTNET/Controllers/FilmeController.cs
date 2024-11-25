635928cf5f1c5c1e062575588eb8405e311a57a3
Victor Lima Bezerra <lima.victoradm@gmail.com>
2024-11-25 18:14:43 -0300

    atualizacao da lista mais tendenciosa do planeta Terra

---
+++ API COM DOTNET/Controllers/FilmeController.cs
@@ -0,0 +1,25 @@
+﻿using API_COM_DOTNET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_COM_DOTNET.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);
+      
+        //10:45
+    }
+
+
+}
---
+++ API COM DOTNET/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_COM_DOTNET.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O titulo deve ser obrigatorio e deve ter no maximo 300 caracteres(incluindo as virgulas).")]
+    [MaxLength(300, ErrorMessage = "O titulo deve ter no maximo 300 caracteres(incluindo as virgulas).")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "A quantidade de genero esta limitada a 50 caracteres(incluindo as virgulas) e deve ser obrigatorio.")]
+    [MaxLength(50, ErrorMessage ="A quantidade de genero esta limitada a 50 caracteres(incluindo as virgulas).")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "O filme deve ter entre 70 a 300 minutos e deve ser obrigatorio.")]
+    [Range(70, 300, ErrorMessage = "O filme deve ter entre 70 a 300 minutos e deve ser obrigatorio.")]
+    public int Duracao { get; set; }
+
+
+}
+
